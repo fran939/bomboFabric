5add92805c8a4c483169a7ae65fd9d647f600183
fran939 <frandl938@gmail.com>
2026-07-27 16:10:07 +0200

    Remove /do path from BomboApiUrl

--- src/client/java/me/bombo/bomboaddons/util/BomboApiUrl.java
+++ src/client/java/me/bombo/bomboaddons/util/BomboApiUrl.java
@@ -7,7 +7,7 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public class BomboApiUrl {
     public static final String MAIN_API_BASE = "https://api.bombo.dpdns.org";
-    public static final String MAIN_WEB_BASE = "https://bombo.dpdns.org/do";
+    public static final String MAIN_WEB_BASE = "https://bombo.dpdns.org";
     public static final String BACKUP_API_BASE = "https://bomboapi.frandl938.workers.dev";
 
     private static final AtomicBoolean useMainDomain = new AtomicBoolean(true);
