b708a7299bfd20bb0e1fa4d536b18ffd3495357e
fran939 <frandl938@gmail.com>
2026-08-23 04:42:21 +0200

    Fix dynamic fabric.mod.json version substitution and upload script

--- upload.ps1
+++ upload.ps1
@@ -2,7 +2,7 @@ $version = Select-String -Path "gradle.properties" -Pattern "mod_version=(.*)" |
 $jarPath = "build/libs/bomboaddons-$version.jar"
 
 Write-Host "Building project for v$version..." -ForegroundColor Yellow
-cmd /c 'gradlew.bat compileClientJava jar -x processResources'
+cmd /c 'gradlew.bat compileClientJava processResources jar'
 
 if (-not $env:GH_TOKEN) {
     try {
